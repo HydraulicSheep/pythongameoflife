913c158450f980de648797dc15e349d64adba009
HydraulicSheep <p.grave@icloud.com>
2018-04-04 13:14:56 +1000

    Added CLI and saved/random boards
    Added a Command Line Interface and enabled playing from a random seed as well as loading a board. Also added error messages.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 import curses
+import random
 
 
 class Cell:
@@ -15,11 +16,11 @@ class Cell:
     def updatecell(self, celllist, boardsize):
         liveneighbours = self.getliveneighbours(celllist, boardsize)
         if liveneighbours < 2 or liveneighbours > 3:
-            self.newlabel = ' '  # Makes cell dead
-        elif liveneighbours == 2 and self.label == ' ':
-            self.newlabel = ' '  # Keeps dead cell with 2 neighbours dead
+            self.newlabel = True  # Makes cell dead
+        elif liveneighbours == 2 and self.label:
+            self.newlabel = True  # Keeps dead cell with 2 neighbours dead
         else:
-            self.newlabel = 0  # Brings/keeps cell alive in other conditions
+            self.newlabel = False  # Keeps cell alive in other conditions
 
     def getliveneighbours(self, celllist, boardsize):
         count = 0
@@ -53,16 +54,20 @@ class Cell:
 class Board:
     celllist = {}  # Dict of rows
 
-    def __init__(self, rows, columns):
+    def __init__(self, rows, columns, random):
         self.rows = rows
         self.columns = columns
+        self.random = random
         self.generate()
 
     def printboard(self):  # Prints the board to the terminal using curses
         for num, row in self.celllist.items():
             line = ''
             for col, cell in enumerate(row):
-                line += str(cell.label)
+                if cell.label:
+                    line += ' '
+                else:
+                    line += '0'
             terminal.addstr(num, 0, line)
         terminal.refresh()
 
@@ -70,7 +75,12 @@ class Board:
         for row in range(self.rows-1):
             self.celllist[row] = []
             for col in range(self.columns):
-                self.celllist[row].append(Cell(row, col, ' '))
+                if self.random:
+                    self.celllist[row].append(Cell(row, col,
+                                                   bool(random.getrandbits(1)
+                                                        )))
+                else:
+                    self.celllist[row].append(Cell(row, col, True))
 
     def updateboard(self):  # Updates each cell and then their label
         for row in self.celllist:
@@ -81,23 +91,107 @@ class Board:
                 cell.updatelabel()
 
 
-if __name__ == "__main__":
+def getfiles():  # Loads all the pattern files
+    files = []
+    print('State files (stored in examples directory):')
+    for file in os.listdir("examples"):
+        if file.endswith(".txt"):
+            files.append(os.path.join("examples/", file))
+    return files
+
+
+def randomboard(rows, columns):  # Sets the cell values to random
+    board = Board(int(rows), int(columns), True)
+    return board
+
+
+def loadboard(rows, columns):  # Handles choosing a file and loading it
+    board = Board(int(rows), int(columns), False)
+    files = getfiles()
+    for num, file in enumerate(files):
+        print(str(num) + ': ' + file)
+    validfile = False
+    file = ''
+    a = input('Type the number of the selected file to load it: ')
+    while not validfile:
+        try:
+            file = files[int(a)]
+            validfile = True
+        except (IndexError, ValueError):
+            a = input('Enter a valid list number to load the selected file: ')
+    content = open(file).readlines()
+    for i, x in enumerate(content):
+        content[i] = str(x).split(',')
+        try:
+            board.celllist[int(content[i][0])][int(content[i][1])].label = 0
+        except ValueError:  # Checks for invalid points
+            print('ERROR: The file must be a series of integer points'
+                  ' on separate lines e.g. 3,1 .' +
+                  'Only points of this format will be loaded')
+        except KeyError:  # Checks for points outside board
+            print('ERROR: The file contains points which are'
+                  ' outside the visible board. Only points inside the board'
+                  ' will be loaded. To possibly fix this in the future,'
+                  ' increase your terminal size.')
+    return board
+
+
+def main():
+    a = ''
+    try:  # Checks that user is using a valid terminal
+        rows, columns = os.popen('stty size', 'r').read().split()
+    except ValueError:
+        print('\033[91m'+'Please switch to a terminal rather than stdin to' +
+              ' run this simulation' + '\033[0m')
+        return 1
+    print("Welcome to Conway's Game of Life in Python")
+    while a.lower() != 'load' and a.lower() != 'random':
+        a = input('To play from a random seed, type random. ' +
+                  'To load, type load: ')
+    if a.lower() == 'random':
+        board = randomboard(rows, columns)
+    else:
+        board = loadboard(rows, columns)
+    while a != '':
+        a = input('\033[92m' +
+                  'To begin the simulation, just press the ENTER KEY.' +
+                  ' To pause the simulation, press the ENTER KEY.' +
+                  '\033[0m')
+    global terminal
     terminal = curses.initscr()  # Opens a curses window
     curses.noecho()
     curses.cbreak()
     terminal.nodelay(1)  # Don't wait for user input later
-    rows, columns = os.popen('stty size', 'r').read().split()
-    board = Board(int(rows), int(columns))
-    board.celllist[6][8].label = 0  # Glider example starting position
-    board.celllist[7][9].label = 0
-    board.celllist[7][10].label = 0
-    board.celllist[8][8].label = 0
-    board.celllist[8][9].label = 0
-    while 1:
-        board.printboard()
-        time.sleep(0.1)
-        board.updateboard()
-        char = terminal.getch()
-        if char == 113:  # Checks for ASCII Char code for q to break loop
+    while True:  # Runs simulation loop
+        try:
+            try:
+                board.printboard()
+            except curses.error:  # Checks for window resize errrors
+                curses.endwin()  # Closes curses window
+                input('\033[91m' +
+                      'PAUSED: The window has been resized which can lead to' +
+                      ' display errors. Please restore the window to its ' +
+                      'original size and press the ENTER KEY to continue. ' +
+                      '\033[0m')
+                terminal = curses.initscr()  # Reopens a curses window
+                terminal.nodelay(1)  # Don't wait for user input later
+            c = terminal.getch()
+            if c == 10:  # Checks for pause button
+                print('\033[91m' +
+                      'PAUSED: Press the ENTER KEY to continue. ' +
+                      '\033[0m')
+                c = 0
+                while c != 10:  # Checks for continue button
+                    c = terminal.getch()
+                curses.endwin()
+                terminal = curses.initscr()  # Reopens a curses window
+                terminal.nodelay(1)  # Don't wait for user input later
+            board.updateboard()
+        except KeyboardInterrupt:  # Ends program with KeyboardInterrupt
             break
-    curses.endwin()  # Closes curses window
+    # Closes curses window
+    curses.endwin()
+
+
+if __name__ == "__main__":
+    main()
